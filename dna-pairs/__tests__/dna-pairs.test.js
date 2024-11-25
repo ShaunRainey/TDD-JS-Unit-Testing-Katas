7576124c7c739e84147ecd946917b1e57ede4976
ShaunRainey <151593013+ShaunRainey@users.noreply.github.com>
2024-11-25 23:21:26 +0000

    Complete task 1

--- dna-pairs/__tests__/dna-pairs.test.js
+++ dna-pairs/__tests__/dna-pairs.test.js
@@ -1,7 +1,28 @@
 const dnaPairs = require('../dna-pairs.js');
 
-describe('dnaPairs()', () => {
-  test('ADD YOUR TEST DESCRIPTION HERE', () => {
-    // add your test assertion(s) here
+describe("dnaPairs()", () => {
+  test('Pairs G to C', () => {
+  const output = dnaPairs("G")
+  expect(output).toEqual([["G","C"]])
+  });
+  test('Pairs C to G', () => {
+    const output = dnaPairs("C")
+    expect(output).toEqual([["C","G"]])
+  });
+  test('Pairs A to T', () => {
+    const output = dnaPairs("A")
+    expect(output).toEqual([["A","T"]])
+  });
+  test('Pairs T to A', () => {
+    const output = dnaPairs("G")
+    expect(output).toEqual([["G","C"]])
+  });
+  test('Returns correct array for multiple characters', () => {
+    let output = dnaPairs("GGG")
+    expect(output).toEqual([["G","C"],["G","C"],["G","C"]])
+    output = dnaPairs("GCA")
+    expect(output).toEqual([["G","C"],["C","G"],["A","T"]])
+    output = dnaPairs("TAT")
+    expect(output).toEqual([["T","A"],["A","T"],["T","A"]])
   });
 });
--- dna-pairs/dna-pairs.js
+++ dna-pairs/dna-pairs.js
@@ -1,6 +1,15 @@
 // Please do not change the name of this function
 function dnaPairs(dna) {
   // Write your code here
-}
+  let newArray = [];
+  for(let i=0; i<dna.length; i++){
+    if(dna[i] === "G"){newArray.push(["G","C"]);}
+    else if(dna[i] === "C"){newArray.push(["C","G"]);}
+    else if(dna[i] === "A"){newArray.push(["A","T"]);}
+    else if(dna[i] === "T"){newArray.push(["T","A"]);}
+    }
+    
+    return newArray;
+  }
 
 module.exports = dnaPairs;
