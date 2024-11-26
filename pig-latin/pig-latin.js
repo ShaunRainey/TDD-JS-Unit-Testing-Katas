d19fe1118e0b63cb48fc237ac46c4e981a5e8543
ShaunRainey <151593013+ShaunRainey@users.noreply.github.com>
2024-11-26 23:49:29 +0000

    Complete task 10

--- pig-latin/pig-latin.js
+++ pig-latin/pig-latin.js
@@ -1,6 +1,21 @@
 // Please do not change the name of this function
 function pigLatin(str) {
   // Your code here
+  const vowels = ["a","e","i","o","u"];
+
+  let returnString = "";
+
+  const wordArray = str.match(/\w+/g)
+
+  wordArray.forEach((word) => {
+    if(!vowels.includes(word[0].toLowerCase())){
+      returnString += word.slice(1) + word[0] + "ay" + " ";
+    } else {
+      returnString += word + "way" + " "
+    }
+  })
+
+  return returnString.slice(0,-1);
 };
 
 module.exports = pigLatin
---
+++ pig-latin/pig-latin.test.js
@@ -0,0 +1,16 @@
+const pigLatin = require("./pig-latin")
+
+describe("pigLatin", () => {
+    test("if a word starts with a consonant (or consecutive consonants), the first consonant(s) is moved to the end, followed by \"ay\" ", () => {
+        const output = pigLatin("northcoders");
+        expect(output).toBe("orthcodersnay")
+    })  
+    test("If a word starts with a vowel, \"way\" is appended to the end ", () => {
+        const output = pigLatin("algorithm");
+        expect(output).toBe("algorithmway")
+    }) 
+    test("Follows the above behaviour for a string of multiple words", () => {
+        const output = pigLatin("keep on coding");
+        expect(output).toBe("eepkay onway odingcay")
+    })
+})
