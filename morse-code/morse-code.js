69b174881967531441106fa050635adcfe428691
ShaunRainey <151593013+ShaunRainey@users.noreply.github.com>
2024-11-26 23:53:37 +0000

    Update morse code

--- morse-code/morse-code.js
+++ morse-code/morse-code.js
@@ -1,6 +1,43 @@
 // Please do not change the name of this function
 function morseCode (str) {
   // Your code here
+const map = {
+A      | .-    |
+B      | -...  |
+C      | -.-.  |
+D      | -..   |
+E      | .     |
+F      | ..-.  |
+G      | --.   |
+H      | ....  |
+I      | ..    |
+J      | .---  |
+K      | -.-   |
+L      | .-..  |
+M      | --    |
+N      | -.    |
+O      | ---   |
+P      | .--.  |
+Q      | --.-  |
+R      | .-.   |
+S      | ...   |
+T      | -     |
+U      | ..-   |
+V      | ...-  |
+W      | .--   |
+X      | -..-  |
+Y      | -.--  |
+Z      | --..  |
+0      | ----- |
+1      | .---- |
+2: ..--- |
+3: ...-- |
+4: ....- |
+5: ..... |
+6: -.... |
+7: --... |
+8: ---.. |
+9: ----. |
 };
 
 module.exports = morseCode;
