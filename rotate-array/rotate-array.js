c49da95968e131f38c4941ff0678ccc17594479b
ShaunRainey <151593013+ShaunRainey@users.noreply.github.com>
2024-11-27 01:15:22 +0000

    Update rotate-array.js

--- rotate-array/rotate-array.js
+++ rotate-array/rotate-array.js
@@ -1,6 +1,22 @@
 // Please do not change the name of this function
 function rotateArray (arr, num) {
   // Your code here
+  let newArray = [];
+
+  for(let i=0; i< arr.length; i++){
+    let currentIndex = i;
+    let newIndex = i + num;
+    if(newIndex > arr.length-1){newIndex = (newIndex % arr.length)}
+    // console.log("i: ", i, " current index of value: ", currentIndex, " new index will be: ", newIndex)
+    newArray[newIndex] = arr[i];
+    // console.log("newArray: ", newArray) 
+  }
+  console.log(arr)
+  return newArray
 };
 
 module.exports =  rotateArray;
+
+console.log(rotateArray([1, 2, 3], 1)) // [3, 1, 2] 
+console.log(rotateArray([1, 2, 3, 4, 5], 3)) // [3, 4, 5, 1, 2]
+
